b2030fcca3fc1232d8a3baee0ca3edb4859478bb
pyyupsk <pyyupsk@proton.me>
2025-05-08 14:59:42 +0700

    feat: enhance district, province, subdistrict, and postal code services with code-based lookups and add new tests

--- tests/test_province_service.py
+++ tests/test_province_service.py
@@ -1,8 +1,9 @@
 import unittest
 from geothai import (
     get_all_provinces,
-    get_province_by_id,
-    get_provinces_by_criterion
+    get_province_by_code,
+    get_provinces_by_criterion,
+    Province
 )
 
 
@@ -12,26 +13,26 @@ class TestProvinceService(unittest.TestCase):
         self.assertIsInstance(provinces, list)
         self.assertGreater(len(provinces), 0)
 
-    def test_should_retrieve_a_province_by_id(self):
-        province_id = 10
-        province = get_province_by_id(province_id)
+    def test_should_retrieve_a_province_by_code(self):
+        province_code = '10'
+        province = get_province_by_code(province_code)
         self.assertIsNotNone(province)
-        self.assertEqual(province['province_id'], province_id)
+        self.assertEqual(province['code'], int(province_code))
 
-    def test_should_return_none_for_an_invalid_province_id(self):
-        invalid_province_id = 99999
-        province = get_province_by_id(invalid_province_id)
+    def test_should_return_none_for_an_invalid_province_code(self):
+        invalid_province_code = '99999'
+        province = get_province_by_code(invalid_province_code)
         self.assertIsNone(province)
 
     def test_should_retrieve_provinces_by_a_specific_criterion(self):
-        criterion = {'province_name_en': 'Bangkok'}
+        criterion: Province = {'name_en': 'Bangkok'}
         provinces = get_provinces_by_criterion(criterion)
         self.assertIsInstance(provinces, list)
         self.assertGreater(len(provinces), 0)
-        self.assertEqual(provinces[0]['province_name_en'], 'Bangkok')
+        self.assertEqual(provinces[0]['name_en'], 'Bangkok')
 
     def test_should_return_an_empty_list_for_a_non_matching_criterion(self):
-        criterion = {'province_name_en': 'Non-Existent Province'}
+        criterion: Province = {'name_en': 'Non-Existent Province'}
         provinces = get_provinces_by_criterion(criterion)
         self.assertIsInstance(provinces, list)
         self.assertEqual(len(provinces), 0)
